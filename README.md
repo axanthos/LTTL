759fcd6eac28d75e878f09d9bc42598125d04d4c
Aris Xanthos <aris.xanthos@unil.ch>
2016-06-19 23:25:30 +0200

    1st standalone version of LTTL

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# LTTL
-LangTech Text Library
+LangTech Text Library (LTTL)
+============================
+
+LTTL is an open-source python package for text processing and analysis.
+
+LTTL consists of: (i) a set of classes for representing segmental analyses of 
+text strings (= "segmentations"), whereby portions of text strings are 
+delineated and possibly annotated with specific labels; (ii) a set of classes 
+for creating and manipulating segmentations, in order to produce either 
+modified version of the original text data, or data tables summarizing 
+properties of these data (e.g. unit frequency, complexity measures, 
+concordances, and so on).
+
+LTTL was designed and implemented by `LangTech Sàrl <http://langtech.ch>`_.
---
+++ setup.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+"""File setup.py
+Copyright 2012-2016 LangTech Sarl (info@langtech.ch)
+---------------------------------------------------------------------------
+This file is part of the LTTL package v2.0.
+
+LTTL v2.0 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+LTTL v2.0 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with LTTL v2.0. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from __future__ import unicode_literals
+
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+__version__ = "1.0.0"
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='LTTL',
+
+    version='2.0.0',
+
+    description='LangTech Text Library (LTTL) for text processing and analysis',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/axanthos/LTTL',
+
+    # Author details
+    author='LangTech Sarl',
+    author_email='info@langtech.ch',
+
+    # Choose your license
+    license='GNU GPL v3',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing :: General',
+        'Topic :: Text Processing :: Linguistic',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+    ],
+
+    keywords=('text mining', 'text analysis', 'text processing'),
+
+    packages=find_packages(exclude=['docs', 'tests']),
+
+    install_requires=[
+        'setuptools',
+        'numpy',
+        'future',
+    ],
+
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
