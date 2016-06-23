16b7353c391cd2817c6ddaba660992e0df124bdb
Aris Xanthos <aris.xanthos@unil.ch>
2016-06-23 10:15:20 +0200

    set version number to 2.0a0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='LTTL',
 
-    version='2.0.0',
+    version='2.0a0',
 
     description='LangTech Text Library (LTTL) for text processing and analysis',
     long_description=long_description,
