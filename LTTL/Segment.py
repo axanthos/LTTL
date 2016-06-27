87e04a7d6c296b516c4371653a34fb335a2e34e9
Julien Eberle <julien.eberle@polylan.ch>
2016-06-27 16:52:23 +0200

    fix bugs

--- LTTL/Segment.py
+++ LTTL/Segment.py
@@ -327,7 +327,7 @@ class Segment(object):
                     start_search = (end_search + start_search) // 2
 
             # In case we need the very first segment...
-            if segmentation[start_search].start == start:
+            if segmentation[start_search].start >= start:
                 start_search -= 1
 
             # Start iterating at this point (start_search always ends up
@@ -372,13 +372,17 @@ class Segment(object):
             # Binary search for start of relevant segments...
             while end_search - start_search > 1:
                 middle = segmentation[(end_search + start_search) // 2]
-                if (middle.start or 0) > start:
+                if (middle.start or 0) >= start:
                     end_search = (end_search + start_search) // 2
                 else:
                     start_search = (end_search + start_search) // 2
 
+            # In case we need the very first segment...
+            if segmentation[start_search].start >= start:
+                start_search -= 1
+
             # start iterating at this point...
-            for i, segment in enumerate(segmentation[start_search:]):
+            for i, segment in enumerate(segmentation[start_search + 1:]):
                 # and stop when we reach the end
                 if (
                     str_index != segment.str_index or
@@ -386,7 +390,7 @@ class Segment(object):
                 ):
                     break
                 if end >= (segment.end or string_length):
-                    ret.append(i + start_search)
+                    ret.append(i + (start_search + 1))
             return ret
         except:
             return list()
